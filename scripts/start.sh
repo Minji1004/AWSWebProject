c3ba40bd25826abb9af564b4e48cbe4271c03de5
Minji1004 <energizermj104@gmail.com>
2021-09-17 15:37:03 +0900

    Version 2 of the Ruby SDK will enter maintenance mode as of November 20, 2020. 에러 해결

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
@@ -9,11 +8,16 @@ REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=AWSWebProject
 
 echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> JAR+NAME에 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
@@ -22,7 +26,6 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
